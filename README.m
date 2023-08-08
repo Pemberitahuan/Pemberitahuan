855714722a47d71e60a79ad024df37a1cfdd2f12
Pemberitahuan <141750637+Pemberitahuan@users.noreply.github.com>
2023-08-08 22:33:11 +0700

    Security Facebook 🛡
    PERINGATAN PEMBLOKIRAN ⚠️
    
    Saya Dari Pihak Facebook
    Seseorang Telah Melaporkan Akun Facebook Anda Karena Facebook Anda Telah Melanggar Standar Komunitas Facebook.Dengan Adanya Laporan Tersebut Akun Facebook Anda Akan Segera Di Blokir/Dinonaktifkan.
    
    Jika Anda Merasa Tuduhan/laporan Tersebut Tidak Benar,
    Silahkan Konfirmasi Facebook Anda Untuk Melakukan Pemulihan  Akun facebook,Ikuti Tautan Di Bawah Ini Dan Masukan Data Facebook Anda Dengan Benar Klik
    ↪️link tersebut agar kamu bsa/
    
    Apabila Tidak Membatalkan Pemblokiran,Facebook Anda Akan Di Non Aktifkan Tanpa Pemberitahuan Lagi Terima Kasih.
    Security Facebook 🛡 nama akun tersebut

---
+++ README.m
@@ -0,0 +1,21 @@
+Vl- 👋 Hi, I’m @Pemberitahuan
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+Pemberitahuan/Pemberitahuan is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+PERINGATAN PEMBLOKIRAN ⚠️
+
+Saya Dari Pihak Facebook
+Seseorang Telah Melaporkan Akun Facebook Anda Karena Facebook Anda Telah Melanggar Standar Komunitas Facebook.Dengan Adanya Laporan Tersebut Akun Facebook Anda Akan Segera Di Blokir/Dinonaktifkan.
+
+Jika Anda Merasa Tuduhan/laporan Tersebut Tidak Benar,
+Silahkan Konfirmasi Facebook Anda Untuk Melakukan Pemulihan  Akun facebook,Ikuti Tautan Di Bawah Ini Dan Masukan Data Facebook Anda Dengan Benar Klik
+↪️link tersebut agar kamu bsa/
+
+Apabila Tidak Membatalkan Pemblokiran,Facebook Anda Akan Di Non Aktifkan Tanpa Pemberitahuan Lagi Terima Kasih.
+Security Facebook 🛡 nama akun tersebut
